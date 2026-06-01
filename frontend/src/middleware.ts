7d683e54fda29605d2a83806d23037094353a2ce
Kuroda Kayn <kurodakayn@outlook.com>
2026-06-01 18:51:22 +0800

    fix(i18n): improve locale detection and redirection logic in middleware for better language handling

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -7,34 +7,38 @@ acceptLanguage.languages(languages);
 export const config = {
   // matcher: '/:lng*'
   matcher: [
-    "/((?!api|_next/static|_next/image|assets|icons|tutorial|favicon.ico|sw.js|site.webmanifest).*)",
+    "/((?!api|_next/static|_next/image|assets|icons|tutorial|favicon\\.ico|robots\\.txt|sitemap\\.xml|sw\\.js|site\\.webmanifest).*)",
   ],
 };
 
 export function middleware(req: NextRequest) {
-  let lng;
-  if (req.cookies.has(cookieName))
+  const localeInPath = languages.find((loc) =>
+    req.nextUrl.pathname.startsWith(`/${loc}`),
+  );
+  const localeInReferer = req.headers.has("referer")
+    ? languages.find((loc) =>
+        new URL(req.headers.get("referer")!).pathname.startsWith(`/${loc}`),
+      )
+    : undefined;
+
+  let lng: string | null | undefined = localeInReferer;
+  if (!lng && req.cookies.has(cookieName))
     lng = acceptLanguage.get(req.cookies.get(cookieName)?.value);
   if (!lng) lng = acceptLanguage.get(req.headers.get("Accept-Language"));
   if (!lng) lng = fallbackLng;
 
   // Redirect if lng in path is not supported
-  if (
-    !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
-    !req.nextUrl.pathname.startsWith("/_next")
-  ) {
-    return NextResponse.redirect(
+  if (!localeInPath && !req.nextUrl.pathname.startsWith("/_next")) {
+    const response = NextResponse.redirect(
       new URL(`/${lng}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url),
     );
+    if (localeInReferer) response.cookies.set(cookieName, localeInReferer);
+    return response;
   }
 
-  if (req.headers.has("referer")) {
-    const refererUrl = new URL(req.headers.get("referer")!);
-    const lngInReferer = languages.find((l) =>
-      refererUrl.pathname.startsWith(`/${l}`),
-    );
+  if (localeInReferer) {
     const response = NextResponse.next();
-    if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
+    response.cookies.set(cookieName, localeInReferer);
     return response;
   }
 
